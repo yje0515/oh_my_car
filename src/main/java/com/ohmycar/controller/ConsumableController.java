849a3e32cfaa82e710bd9a1ad715d7552e88a25c
misosiruda <81164760+misosiruda@users.noreply.github.com>
2024-05-07 11:53:59 +0900

    교체 주기 반영 해서 read 에 넣어놨음

--- src/main/java/com/ohmycar/controller/ConsumableController.java
+++ src/main/java/com/ohmycar/controller/ConsumableController.java
@@ -1,9 +1,16 @@
 package com.ohmycar.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ohmycar.domain.ConsumableVO;
+import com.ohmycar.service.ConsumableCycle;
 import com.ohmycar.service.ConsumableService;
 
 import lombok.extern.log4j.Log4j;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,12 +28,74 @@ public class ConsumableController {
         this.consumableService = consumableService;
     }
 
+    private String calcChangeDay(String before, String cycle, char op) {
+        if (op == 'y') {
+            int next = (int) Double.parseDouble(cycle) * 12;
+            LocalDate date = LocalDate.parse(before, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            date = date.plusMonths(next);
+            return date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        } else if (op == 'k') {
+            double beforeNum = Double.parseDouble(before);
+            double cycleNum = Double.parseDouble(cycle);
+            double next = beforeNum + cycleNum;
+            return String.valueOf(next);
+        } else {
+            return "unknown";
+        }
+    }
+
     @GetMapping("/read")
-    public void read(@RequestParam("carId") String carId, Model model) {
+    public void read(@RequestParam("carId") String carId, Model model) throws Exception {
         log.info("reading..............................................................");
         ConsumableVO vo = consumableService.read(carId);
         log.info(vo);
         model.addAttribute("consumable", vo);
+
+        InputStream input = new FileInputStream(
+                "c:/SpringWorkSpace/oh_my_car/src/main/webapp/resources/json/consumableCycle.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        ConsumableCycle consumableCycle = objectMapper.readValue(input, ConsumableCycle.class);
+
+        ConsumableVO next = consumableService.read(carId);
+        String st = consumableCycle.getAirConFilter();
+        next.setAirConFilter(
+                calcChangeDay(next.getAirConFilter(), st.substring(0, st.length() - 1), st.charAt(st.length() - 1)));
+        st = consumableCycle.getBattery();
+        next.setBattery(
+                calcChangeDay(next.getBattery(), st.substring(0, st.length() - 1), st.charAt(st.length() - 1)));
+        st = consumableCycle.getBreakOil();
+        next.setBreakOil(
+                calcChangeDay(next.getBreakOil(), st.substring(0, st.length() - 1), st.charAt(st.length() - 1)));
+        st = consumableCycle.getBreakPad();
+        next.setBreakPad(
+                calcChangeDay(next.getBreakPad(), st.substring(0, st.length() - 1), st.charAt(st.length() - 1)));
+        st = consumableCycle.getCoolingWater();
+        next.setCoolingWater(
+                calcChangeDay(next.getCoolingWater(), st.substring(0, st.length() - 1), st.charAt(st.length() - 1)));
+        st = consumableCycle.getEngineOil();
+        next.setEngineOil(
+                calcChangeDay(next.getEngineOil(), st.substring(0, st.length() - 1), st.charAt(st.length() - 1)));
+        st = consumableCycle.getFluidClutch();
+        next.setFluidClutch(
+                calcChangeDay(next.getFluidClutch(), st.substring(0, st.length() - 1), st.charAt(st.length() - 1)));
+        st = consumableCycle.getPowerSteeringOil();
+        next.setPowerSteeringOil(
+                calcChangeDay(next.getPowerSteeringOil(), st.substring(0, st.length() - 1),
+                        st.charAt(st.length() - 1)));
+        st = consumableCycle.getSparkPlug();
+        next.setSparkPlug(
+                calcChangeDay(next.getSparkPlug(), st.substring(0, st.length() - 1), st.charAt(st.length() - 1)));
+        st = consumableCycle.getTire();
+        next.setTire(
+                calcChangeDay(next.getTire(), st.substring(0, st.length() - 1), st.charAt(st.length() - 1)));
+        st = consumableCycle.getTransmissionOil();
+        next.setTransmissionOil(
+                calcChangeDay(next.getTransmissionOil(), st.substring(0, st.length() - 1), st.charAt(st.length() - 1)));
+        st = consumableCycle.getWiper();
+        next.setWiper(
+                calcChangeDay(next.getWiper(), st.substring(0, st.length() - 1), st.charAt(st.length() - 1)));
+
+        model.addAttribute("changed", next);
     }
 
 }
---
+++ src/main/java/com/ohmycar/service/ConsumableCycle.java
@@ -0,0 +1,21 @@
+
+package com.ohmycar.service;
+
+import lombok.Data;
+
+@Data
+public class ConsumableCycle {
+
+    private String engineOil;
+    private String breakOil;
+    private String breakPad;
+    private String wiper;
+    private String tire;
+    private String airConFilter;
+    private String sparkPlug;
+    private String battery;
+    private String coolingWater;
+    private String transmissionOil;
+    private String powerSteeringOil;
+    private String fluidClutch;
+}
