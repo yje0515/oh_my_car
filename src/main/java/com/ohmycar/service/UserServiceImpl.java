6f90c0d41a186069595ef4e01e8877a43cda4407
yje0515 <155126222+yje0515@users.noreply.github.com>
2024-05-30 11:36:16 +0900

    userDelete 수정

--- src/main/java/com/ohmycar/service/UserServiceImpl.java
+++ src/main/java/com/ohmycar/service/UserServiceImpl.java
@@ -6,7 +6,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ohmycar.domain.AuthVO;
+import com.ohmycar.domain.CarVO;
 import com.ohmycar.domain.UserVO;
+import com.ohmycar.mapper.CarMapper;
+import com.ohmycar.mapper.ConsumableMapper;
 import com.ohmycar.mapper.UserMapper;
 
 @Service
@@ -14,11 +17,17 @@ public class UserServiceImpl implements UserService {
 
 	private final UserMapper userMapper;
 
+	private final CarMapper carMapper;
+	
+	private final ConsumableMapper consumableMapper;
+	
 	private final PasswordEncoder passwordEncoder;
 
-	public UserServiceImpl(UserMapper userMapper, PasswordEncoder passwordEncoder) {
+	public UserServiceImpl(UserMapper userMapper, PasswordEncoder passwordEncoder,CarMapper carMapper,ConsumableMapper consumableMapper) {
 		this.userMapper = userMapper;
 		this.passwordEncoder = passwordEncoder;
+		this.carMapper=carMapper;
+		this.consumableMapper=consumableMapper;
 	}
 
 	@Override
@@ -69,6 +78,14 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public void deleteUser(String userId) {
+		//carList=>carVO =>userid로 carid받아서 consumable 삭제
+		List<CarVO> carList = carMapper.getCarsByUserId(userId);
+		for(CarVO carVO : carList) {
+			String carId = carVO.getCarId();
+			consumableMapper.delete(carId);
+			carMapper.deleteCar(carId);
+		}
+		
 		userMapper.deletePersistentLogins(userId); // 자동로그인정보삭제
 		userMapper.deleteUserAuth(userId);// 자식테이블(fk) 먼저 삭제
 		userMapper.deleteUser(userId);// 부모테이블(pk)나중에 삭제
